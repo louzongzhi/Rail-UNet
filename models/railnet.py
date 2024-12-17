ce833c6ef67c0c04e306abca2843056fc395e93d
louzongzhi <2871561809@qq.com>
2024-12-17 12:43:06 +0800

    Refactor RailNet model, simplify and optimize code

--- models/railnet.py
+++ models/railnet.py
@@ -5,94 +5,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-import torch.nn.modules
 from turtle import forward
-
-
-class vgg16bn(torch.nn.Module):
-    def __init__(self,pretrained = False):
-        super(vgg16bn,self).__init__()
-        model = list(torchvision.models.vgg16_bn(pretrained=pretrained).features.children())
-        model = model[:33]+model[34:43]
-        self.model = torch.nn.Sequential(*model)
-
-    def forward(self,x):
-        return self.model(x)
-    
-
-class resnet(torch.nn.Module):
-    def __init__(self,layers,pretrained = False):
-        super(resnet,self).__init__()
-        if layers == '18':
-            model = torchvision.models.resnet18(pretrained=pretrained)
-        elif layers == '34':
-            model = torchvision.models.resnet34(pretrained=pretrained)
-        elif layers == '50':
-            model = torchvision.models.resnet50(pretrained=pretrained)
-        elif layers == '101':
-            model = torchvision.models.resnet101(pretrained=pretrained)
-        elif layers == '152':
-            model = torchvision.models.resnet152(pretrained=pretrained)
-        elif layers == '50next':
-            model = torchvision.models.resnext50_32x4d(pretrained=pretrained)
-        elif layers == '101next':
-            model = torchvision.models.resnext101_32x8d(pretrained=pretrained)
-        elif layers == '50wide':
-            model = torchvision.models.wide_resnet50_2(pretrained=pretrained)
-        elif layers == '101wide':
-            model = torchvision.models.wide_resnet101_2(pretrained=pretrained)
-        else:
-            raise NotImplementedError
-        self.conv1 = model.conv1
-        self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
-        self.layer1 = model.layer1
-        self.layer2 = model.layer2
-        self.layer3 = model.layer3
-        self.layer4 = model.layer4
-
-    def forward(self,x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x2 = self.layer2(x)
-        x3 = self.layer3(x2)
-        x4 = self.layer4(x3)
-        return x,x2,x3,x4
-
 
 class conv_bn_relu(nn.Module):
     def __init__(self, in_channels, out_channels, upsample=0):
-        super(conv_bn_relu,self).__init__()
-        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation = 1, bias = False)
+        super(conv_bn_relu, self).__init__()
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, dilation=1, bias=False)
         self.bn = torch.nn.BatchNorm2d(out_channels)
         self.relu = torch.nn.ReLU()
         self.upsample = upsample
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         x = self.relu(x)
         if self.upsample:
             x = F.interpolate(x, scale_factor=2, mode="bilinear", align_corners=True)
         return x
-
-
-class SegmentationBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, n_upsamples=0):
-        super().__init__()
-        blocks = [conv_bn_relu(in_channels, out_channels, upsample=bool(n_upsamples))]
-        if n_upsamples > 1:
-            for _ in range(1, n_upsamples):
-                blocks.append(conv_bn_relu(out_channels, out_channels, upsample=True))
-        self.block = nn.Sequential(*blocks)
-
-    def forward(self, x):
-        return self.block(x)
-
 
 class FPNBlock(nn.Module):
     def __init__(self, pyramid_channels, skip_channels):
@@ -105,7 +34,6 @@ class FPNBlock(nn.Module):
         x = x + skip
         return x
 
-
 class SegmentationBlock(nn.Module):
     def __init__(self, in_channels, out_channels, n_upsamples=0):
         super().__init__()
@@ -118,7 +46,6 @@ class SegmentationBlock(nn.Module):
     def forward(self, x):
         return self.block(x)
 
-
 class SegmentationHead(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=4):
         super(SegmentationHead, self).__init__()
@@ -130,6 +57,40 @@ class SegmentationHead(torch.nn.Module):
         x = self.upsampling(x)
         return x
 
+class resnet(torch.nn.Module):
+    def __init__(self, layers, pretrained=False):
+        super(resnet, self).__init__()
+        model_dict = {
+            '18': torchvision.models.resnet18(pretrained=pretrained),
+            '34': torchvision.models.resnet34(pretrained=pretrained),
+            '50': torchvision.models.resnet50(pretrained=pretrained),
+            '101': torchvision.models.resnet101(pretrained=pretrained),
+            '152': torchvision.models.resnet152(pretrained=pretrained),
+            '50next': torchvision.models.resnext50_32x4d(pretrained=pretrained),
+            '101next': torchvision.models.resnext101_32x8d(pretrained=pretrained),
+            '50wide': torchvision.models.wide_resnet50_2(pretrained=pretrained),
+            '101wide': torchvision.models.wide_resnet101_2(pretrained=pretrained)
+        }
+        model = model_dict[layers]
+        self.conv1 = model.conv1
+        self.bn1 = model.bn1
+        self.relu = model.relu
+        self.maxpool = model.maxpool
+        self.layer1 = model.layer1
+        self.layer2 = model.layer2
+        self.layer3 = model.layer3
+        self.layer4 = model.layer4
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x2 = self.layer2(x)
+        x3 = self.layer3(x2)
+        x4 = self.layer4(x3)
+        return x, x2, x3, x4
 
 class parsingNet(torch.nn.Module):
     def __init__(self, size=(288, 800), pretrained=True, backbone='50', cls_dim=(37, 10, 4), n_classes=9, n_channels=3, bilinear=False):
@@ -137,47 +98,37 @@ class parsingNet(torch.nn.Module):
         self.size = size
         self.w = size[0]
         self.h = size[1]
-        self.cls_dim = cls_dim # (num_gridding, num_cls_per_lane, num_of_lanes)
+        self.cls_dim = cls_dim
         self.n_classes = n_classes
         self.n_channels = n_channels
         self.bilinear = bilinear
-        # num_cls_per_lane is the number of row anchors
         self.total_dim = np.prod(cls_dim)
 
-        # input : nchw,
-        # output: (w+1) * sample_rows * 4 
         self.model = resnet(backbone, pretrained=pretrained)
 
-        # Lateral layers
-        self.p5 = conv_bn_relu(512, 128) if backbone in ['34','18'] else conv_bn_relu(2048, 128)
-        self.p4 = FPNBlock(128, 256) if backbone in ['34','18'] else FPNBlock(128, 1024)
-        self.p3 = FPNBlock(128, 128) if backbone in ['34','18'] else FPNBlock(128, 512)
-        self.p2 = FPNBlock(128, 64) if backbone in ['34','18'] else FPNBlock(128, 256)
+        self.p5 = conv_bn_relu(512, 128) if backbone in ['34', '18'] else conv_bn_relu(2048, 128)
+        self.p4 = FPNBlock(128, 256) if backbone in ['34', '18'] else FPNBlock(128, 1024)
+        self.p3 = FPNBlock(128, 128) if backbone in ['34', '18'] else FPNBlock(128, 512)
+        self.p2 = FPNBlock(128, 64) if backbone in ['34', '18'] else FPNBlock(128, 256)
 
-        # Smooth layers
         self.smooth5 = SegmentationBlock(128, 128, n_upsamples=3)
         self.smooth4 = SegmentationBlock(128, 128, n_upsamples=2)
         self.smooth3 = SegmentationBlock(128, 128, n_upsamples=1)
         self.smooth2 = SegmentationBlock(128, 128, n_upsamples=0)
 
-        # Final layers output : n, num_of_lanes+1, h, w
         self.finallayer = SegmentationHead(128*4, self.n_classes)
 
         initialize_weights(self.p5, self.p4, self.p3, self.p2,
                             self.smooth5, self.smooth4, self.smooth3, self.smooth2, self.finallayer)
 
     def forward(self, x):
-        # n c h w - > n 2048 sh sw
-        # Bottom-up
         c2, c3, c4, c5 = self.model(x)
 
-        # Top-down
         p5 = self.p5(c5)
         p4 = self.p4(p5, c4)
         p3 = self.p3(p4, c3)
         p2 = self.p2(p3, c2)
         
-        # Smooth
         p5 = self.smooth5(p5)
         p4 = self.smooth4(p4)
         p3 = self.smooth3(p3)
@@ -205,8 +156,8 @@ def real_init_weights(m):
         elif isinstance(m, torch.nn.BatchNorm2d):
             torch.nn.init.constant_(m.weight, 1)
             torch.nn.init.constant_(m.bias, 0)
-        elif isinstance(m,torch.nn.Module):
+        elif isinstance(m, torch.nn.Module):
             for mini_m in m.children():
                 real_init_weights(mini_m)
         else:
-            print('unkonwn module', m)
+            print('unknown module', m)
