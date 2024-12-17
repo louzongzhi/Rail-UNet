321432fa6ba0caecbcaa9cf53e0c343ecdef909b
louzongzhi <2871561809@qq.com>
2024-12-17 12:39:13 +0800

    Refactor RailNet model, remove Deeplabv3plus

--- models/railnet.py
+++ models/railnet.py
@@ -1,9 +1,13 @@
-from turtle import forward
-import torch, sys
-
-import torch,pdb
+import sys
+import pdb
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torchvision
 import torch.nn.modules
+from turtle import forward
+
 
 class vgg16bn(torch.nn.Module):
     def __init__(self,pretrained = False):
@@ -11,10 +15,11 @@ class vgg16bn(torch.nn.Module):
         model = list(torchvision.models.vgg16_bn(pretrained=pretrained).features.children())
         model = model[:33]+model[34:43]
         self.model = torch.nn.Sequential(*model)
-        
+
     def forward(self,x):
         return self.model(x)
-        
+    
+
 class resnet(torch.nn.Module):
     def __init__(self,layers,pretrained = False):
         super(resnet,self).__init__()
@@ -38,7 +43,6 @@ class resnet(torch.nn.Module):
             model = torchvision.models.wide_resnet101_2(pretrained=pretrained)
         else:
             raise NotImplementedError
-        
         self.conv1 = model.conv1
         self.bn1 = model.bn1
         self.relu = model.relu
@@ -58,10 +62,7 @@ class resnet(torch.nn.Module):
         x3 = self.layer3(x2)
         x4 = self.layer4(x3)
         return x,x2,x3,x4
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+
 
 class conv_bn_relu(nn.Module):
     def __init__(self, in_channels, out_channels, upsample=0):
@@ -79,20 +80,19 @@ class conv_bn_relu(nn.Module):
             x = F.interpolate(x, scale_factor=2, mode="bilinear", align_corners=True)
         return x
 
+
 class SegmentationBlock(nn.Module):
     def __init__(self, in_channels, out_channels, n_upsamples=0):
         super().__init__()
-
         blocks = [conv_bn_relu(in_channels, out_channels, upsample=bool(n_upsamples))]
-
         if n_upsamples > 1:
             for _ in range(1, n_upsamples):
                 blocks.append(conv_bn_relu(out_channels, out_channels, upsample=True))
-
         self.block = nn.Sequential(*blocks)
 
     def forward(self, x):
         return self.block(x)
+
 
 class FPNBlock(nn.Module):
     def __init__(self, pyramid_channels, skip_channels):
@@ -105,25 +105,23 @@ class FPNBlock(nn.Module):
         x = x + skip
         return x
 
+
 class SegmentationBlock(nn.Module):
     def __init__(self, in_channels, out_channels, n_upsamples=0):
         super().__init__()
-
         blocks = [conv_bn_relu(in_channels, out_channels, upsample=bool(n_upsamples))]
-
         if n_upsamples > 1:
             for _ in range(1, n_upsamples):
                 blocks.append(conv_bn_relu(out_channels, out_channels, upsample=True))
-
         self.block = nn.Sequential(*blocks)
 
     def forward(self, x):
         return self.block(x)
 
+
 class SegmentationHead(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=4):
         super(SegmentationHead, self).__init__()
-
         self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
         self.upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
 
@@ -131,11 +129,11 @@ class SegmentationHead(torch.nn.Module):
         x = self.conv2d(x)
         x = self.upsampling(x)
         return x
-        
+
+
 class parsingNet(torch.nn.Module):
     def __init__(self, size=(288, 800), pretrained=True, backbone='50', cls_dim=(37, 10, 4), n_classes=9, n_channels=3, bilinear=False):
         super(parsingNet, self).__init__()
-
         self.size = size
         self.w = size[0]
         self.h = size[1]
@@ -194,7 +192,6 @@ def initialize_weights(*models):
         real_init_weights(model)
 
 def real_init_weights(m):
-
     if isinstance(m, list):
         for mini_m in m:
             real_init_weights(mini_m)
